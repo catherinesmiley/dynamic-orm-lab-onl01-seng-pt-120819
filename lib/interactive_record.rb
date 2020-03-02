7c53248c950eefb74880a26d722fbff7745b0434
Catherine Smiley <catherinesmiley@gmail.com>
2020-03-02 17:56:58 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -49,7 +49,7 @@ class InteractiveRecord
     SQL
     
     DB[:conn].execute(sql)
-    @id = DB[:conn].execute("SELECT FROM last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end 
   
 end
